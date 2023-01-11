79070ecb486b00f1737790d9ad2f41041fcb4ccd
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2023-01-11 19:46:19 +0200

    Fix path not defined

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const path = require('path')
 const PORT = process.env.PORT || 3030
 
 const toyService = require('./services/toy.service.js')
