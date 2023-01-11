55eb8118f07806b65f8e25292a6e3b9698148596
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2023-01-11 18:11:16 +0200

    Add mobile menu

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -1,16 +1,26 @@
 import { NavLink } from 'react-router-dom'
 
 export function AppHeader() {
+
+    function onToggleMenu(){
+        document.body.classList.toggle('menu-open')
+    }
+
+    function onCloseMenu() {
+        document.body.classList.remove('menu-open')
+    }
+
     return (
         <header className="main-header full main-layout">
             <div className='header-container'>
                 <div className="brand"><h1>Tois</h1></div>
-                <nav>
-                    <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/about'>About</NavLink>
-                    <NavLink to='/dashboard'>Dashboard</NavLink>
-                    <NavLink to='/toy'>Toys</NavLink>
+                <nav className='main-layout'>
+                    <NavLink onClick={onCloseMenu} to='/'>Home</NavLink>
+                    <NavLink onClick={onCloseMenu} to='/about'>About</NavLink>
+                    <NavLink onClick={onCloseMenu} to='/dashboard'>Dashboard</NavLink>
+                    <NavLink onClick={onCloseMenu} to='/toy'>Toys</NavLink>
                 </nav>
+                <div onClick={onToggleMenu} className="hamburger">☰</div>
             </div>
         </header>
     )
