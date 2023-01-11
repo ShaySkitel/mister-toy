a1f36d1794aaaa2d52d19556250f57aace25487c
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2023-01-11 19:20:04 +0200

    Style toy details page

--- frontend/src/views/toy-details.jsx
+++ frontend/src/views/toy-details.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useNavigate, useParams } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 import { toyService } from "../services/toy.service.js"
 import { removeToy } from "../store/actions/toy.action.js"
 
@@ -20,15 +20,23 @@ export function ToyDetails() {
     return (
         toy && (
             <section className="toy-details">
-                <h2>{toy.name}</h2>
-                <p>{toy.labels.join(', ')}</p>
-                <img src={`https://robohash.org/${toy.name}?set=set3`} alt={toy.name} />
-                <h3>Price ${toy.price}</h3>
+                <Link className="btn" to="/toy">Go back</Link>
+                <div className="toy-img">
+                    <img src={`https://robohash.org/${toy.name}?set=set3`} alt={toy.name} />
+                </div>
+
+                <div className="toy-info">
+                    <h2>{toy.name}</h2>
+                    <p>{toy.labels.join(', ')}</p>
+                    <h3>Price ${toy.price}</h3>
+                    <section className="toy-buttons">
+                        <button className="btn" onClick={() => navigate(`/toy/edit/${toy._id}`)}>Edit</button>
+                        <button className="btn" onClick={onRemoveToy}>Remove</button>
+                    </section>
+                </div>
+
                 <h3>Created at {new Date(toy.createdAt).toDateString()}</h3>
-                <section className="toy-buttons">
-                    <button onClick={() => navigate(`/toy/edit/${toy._id}`)}>Edit</button>
-                    <button onClick={onRemoveToy}>Remove</button>
-                </section>
+
             </section>
         )
     )
